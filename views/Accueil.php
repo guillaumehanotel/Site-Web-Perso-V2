79f158e24dab83f72f22b2c129358a2f7f688072
guillaumehanotel <guiguihanotel@orange.fr>
2017-02-27 13:41:15 +0100

    bordure partielle

--- views/Accueil.php
+++ views/Accueil.php
@@ -28,48 +28,112 @@
         </div>
         
         
+        <div id="apropos" class="divider"></div>
+        <h3>A Propos de Moi...</h3>
+        
+        <div class="row">
+            <p class="col s8 offset-s2"><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam quos laboriosam ut molestias laudantium voluptates excepturi ullam adipisci soluta, et, pariatur dolorum quaerat reiciendis corrupti perspiciatis, explicabo nostrum, iure minima.</span>
+            <span>Quam sit numquam blanditiis quos amet velit eum at dolorem ea voluptatum quaerat itaque magnam voluptates sint placeat, voluptatibus et id, hic ad sapiente vel ut quasi. Iure, quasi, et.</span>
+            <span>Nisi voluptates vel quisquam autem quo. Odit mollitia eaque placeat inventore adipisci alias quo aut quos molestias, deleniti quia impedit debitis, quis dignissimos molestiae voluptatibus voluptatum dolorem veniam. Rem, quod.</span>
+            <span>Est tenetur vel quos quis eligendi corporis, sit cum numquam porro labore eum, maxime atque a odit modi? Officia consequatur dolor et porro dolorum nesciunt neque quas magnam obcaecati, quibusdam!</span>
+            <span>Soluta odio adipisci dicta distinctio? Aperiam magnam non quis numquam voluptate earum, laudantium dolores enim, eveniet magni, qui harum voluptates. Voluptatum illum excepturi quibusdam, ab ad eos beatae ea nihil.</span></p>
+        
+        </div>
         
         
+        
+        
+        <div class="divider"></div>
 
     </div>
     
     
     <!-- ======= EXPERIENCES PROFESSIONNELLES ========= -->
     
-    <div id="section1" class="section">
+    <div id="section1" class="section container">
         <h2 id="titre1">EXPERIENCES PROFESSIONNELLES</h2>
         <div id="toplink1" class="toplink"></div>
+        
+        
+        <div class="expe">
+           
+           
+            
+            <div id="border-top">
+                <div id="top-left"></div>
+                <div id="top-right"></div>    
+            </div>
+            
+            
+            
+            <div id="content">
+                
+                <p>ef</p>
+                
+            </div>
+            
+            
+            
+            
+            
+            <div id="border-bottom">
+                <div id="bottom-left"></div>
+                <div id="bottom-right"></div>
+            </div>
+            
+
+        </div>
+        
+        
+        
+        
+        
+        <div class="divider"></div>
     </div>
     
     
     
-    <div id="section2" class="section">
+    <div id="section2" class="section container">
        
        <h2 id="titre2">REALISATIONS</h2>
        <div id="toplink2" class="toplink"></div>
+       
+       
+       
+       <div class="divider"></div>
     </div>
     
     
     
-    <div id="section3" class="section">
+    <div id="section3" class="section container">
         
         <h2 id="titre3">COMPETENCES</h2>
         <div id="toplink3" class="toplink"></div>
+        
+        
+        <div class="divider"></div>
     </div>
     
     
     
-    <div id="section4" class="section">
+    <div id="section4" class="section container">
         
         <h2 id="titre4">FORMATIONS</h2>
         <div id="toplink4" class="toplink"></div>
+        
+        
+        <div class="divider"></div>
     </div>
     
     
-    <div id="section5" class="section">
+    <div id="section5" class="section container">
         
         <h2 id="titre5">CONTACT</h2>
         <div id="toplink5" class="toplink"></div>
+        
+        
+        
+        <div class="divider"></div>
     </div>
     
 
